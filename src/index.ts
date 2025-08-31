673441138a68bb031414b5677dca4b0b4829236c
acopier <filip.hrouda@outlook.cz>
2025-08-31 07:59:17 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,11 @@ import "../seyfert.config.js";
 import { HttpClient, type ParseClient } from "seyfert";
 import { GenericAdapter } from "@slipher/generic-adapter";
 
-import Ping from "./commands/ping.js";
-
 const client = new HttpClient();
 const adapter = new GenericAdapter(client);
 
 await adapter.start();
-client.start().then(async () => {
-  // we need to load commands manually
-  // @ts-expect-error
-  await client.commands.set([Ping]);
-});
+await client.start();
 
 export default {
   fetch(req: Request) {
